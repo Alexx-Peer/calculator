5526a8177df9175e5b573cb29925ae54d3936919
Alexander <alexxpeer@icloud.com>
2025-02-17 03:46:29 -0500

    did the dang thang

--- calculator.js
+++ calculator.js
@@ -13,8 +13,40 @@ const operation = (process.argv[3]).toLowerCase();
 const num2 = parseFloat(process.argv[4]);
 
 let result = `Solving ${num1} ${operation} ${num2}`;
-
 // Implementation goes here
+if (typeof num1 !== "number" || typeof num2 !== "number") {
+  console.log('invalid number');
+  process.exit(1);
+}
+
+if (operation === 'add') {
+  result = add(num1, num2);
+}
+else if (operation === 'subtract') {
+  result = subtract(num1, num2);
+}
+else if (operation === 'times' || operation === 'multiply') {
+  result = times(num1, num2);
+}
+else if (operation === 'divide') {
+  result = divide(num1, num2);
+}
+else if (operation === 'modulus' || operation === 'remainder') {
+  result = modulus(num1, num2);
+}
+else if (operation === 'power') {
+  result = power(num1, num2);
+
+}
+else {
+  console.error('unknown operation');
+  process.exit(1);
+}
+
+if (isNaN(result)) {
+  console.error('invalid number');
+  process.exit(1);
+}
 // if you get an error, exit with an error of 1 by calling process.exit(1);
 
 
